f04fc2876af876c17823099b23e2e8227956a84f
shicks <shicks255@yahoo.com>
2019-08-28 21:35:25 -0500

    working on more koans

--- Collections/All Any and other predicates/src/Task.kt
+++ Collections/All Any and other predicates/src/Task.kt
@@ -1,11 +1,15 @@
+import java.util.function.Predicate
+
 // Return true if all customers are from the given city
-fun Shop.checkAllCustomersAreFrom(city: City): Boolean = TODO()
+fun Shop.checkAllCustomersAreFrom(city: City): Boolean = this.customers.all { matcher(it, city) }
 
 // Return true if there is at least one customer from the given city
-fun Shop.hasCustomerFrom(city: City): Boolean = TODO()
+fun Shop.hasCustomerFrom(city: City): Boolean = this.customers.any { matcher(it, city) }
 
 // Return the number of customers from the given city
-fun Shop.countCustomersFrom(city: City): Int = TODO()
+fun Shop.countCustomersFrom(city: City): Int = this.customers.count { matcher(it, city) }
 
 // Return a customer who lives in the given city, or null if there is none
-fun Shop.findAnyCustomerFrom(city: City): Customer? = TODO()
+fun Shop.findAnyCustomerFrom(city: City): Customer? = this.customers.firstOrNull{ matcher(it, city) }
+
+val matcher: (Customer, City) -> Boolean = {customer, city -> customer.city == city  }
